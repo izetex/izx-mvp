5b0ca7ed810506b451941a843d26e01b940b4eb3
astudnev <aleksey@wondersoft.ru>
2017-07-18 18:56:56 +0300

    has error

--- routes/balance.js
+++ routes/balance.js
@@ -5,19 +5,17 @@ var HDWalletProvider = require("truffle-hdwallet-provider");
 
 var router = express.Router();
 
-var provider, web3;
-
 var mnemonic = 'satisfy test used phone glue air pyramid aerobic heavy nation moon syrup';
 
-
 router.use(function timeLog(req, res, next) {
-    provider = new HDWalletProvider(mnemonic, "http://node1.izx.io:8545");
-    web3 = new Web3(provider);
     next();
 });
 
 
 router.get('/', function(req, res, next) {
+
+    var provider = new HDWalletProvider(mnemonic, "http://node1.izx.io:8545");
+    var web3 = new Web3(provider);
 
     web3.eth.getBalance( provider.address,
         function(error, result){
